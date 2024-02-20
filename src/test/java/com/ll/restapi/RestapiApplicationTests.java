90e18430d67ed5455e87794c0e85b847179094bb
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-20 21:12:56 +0900

    feat: application.yml 설정

--- src/test/java/com/ll/restapi/RestapiApplicationTests.java
+++ src/test/java/com/ll/restapi/RestapiApplicationTests.java
@@ -2,12 +2,11 @@ package com.ll.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestapiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
