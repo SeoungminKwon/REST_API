b32c2bc5aa46dadf8800122bf01622e222cfde4c
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-21 10:13:12 +0900

    feat: 테스트 케이스에 날짜 관련 데이터는 패턴으로 체크

--- src/test/java/com/ll/restapi/domain/article/article/controller/ApiV1ArticleControllerTest.java
+++ src/test/java/com/ll/restapi/domain/article/article/controller/ApiV1ArticleControllerTest.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -17,9 +19,13 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest //스프링 부트 테스트에 필요한 의존성 주입과 애플리케이션 컨텍스트 로딩을 위한 어노테이션
 @AutoConfigureMockMvc //MockMvc 인스턴스를 자동으로 구성하고, 주입하기 위한 어노테이션
 @ActiveProfiles("test") //Test 프로필 활성화
+@Transactional
 class ApiV1ArticleControllerTest {
     @Autowired //MockMvc인스턴스 자동 주입
     private MockMvc mvc;
+
+    //날짜 패턴 정규식
+    private static final String DATE_PATTERN = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.?\\d{0,7}";
 
     @Test
     @DisplayName("GET /api/v1/articles")
@@ -34,6 +40,11 @@ class ApiV1ArticleControllerTest {
                 .andExpect(status().isOk()) //HTTP 상태코드 검증
                 .andExpect(handler().handlerType(ApiV1ArticleController.class)) //요청 처리 클래스 ApiV1ArticleController인지 검증
                 .andExpect(jsonPath("$.data.items[0].id", is(10))) //JSON 응답의 특정경로 값 검증
-                .andExpect(jsonPath("$.data.items[0].title", is("제목10")));//JSON 응답의 특정경로 값 검증
+                .andExpect(jsonPath("$.data.items[0].createDate", matchesPattern(DATE_PATTERN)))
+                .andExpect(jsonPath("$.data.items[0].modifyDate", matchesPattern(DATE_PATTERN)))
+                .andExpect(jsonPath("$.data.items[0].authorId", is(1)))
+                .andExpect(jsonPath("$.data.items[0].authorName", is("admin")))
+                .andExpect(jsonPath("$.data.items[0].title", is("제목10")))//JSON 응답의 특정경로 값 검증
+                .andExpect(jsonPath("$.data.items[0].body", is("내용10")));
     }
 }
