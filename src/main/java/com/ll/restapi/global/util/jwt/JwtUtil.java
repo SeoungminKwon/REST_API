580fe6c0b081457892dc0aa2ad7dd7711ec0bf09
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-02-22 10:20:35 +0900

    feat: JwtUtil 클래스 도입, JWT 토큰은 데이터와 서명으로 구성되어있다.

---
+++ src/main/java/com/ll/restapi/global/util/jwt/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.ll.restapi.global.util.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+    public static String encode(Map< String, String > data) {
+        //jwt 내용 ?
+        //Claims 객체는 JWT 페이로드에 들어갈 여러 claims를 설정 할 수 있다.
+        Claims claims = Jwts
+                .claims()
+                .setSubject("RESTAPI jwt") //토큰 이름
+                .add("type", data)
+                .build();
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 1000 * 60 * 5); // 5분
+
+        return Jwts.builder()
+                .setClaims(claims) //만들어둔 Claims 객체를 설정
+                .setIssuedAt(now) // JWT 발행시간을 현제 시간으로 설정
+                .setExpiration(validity) //만료 시간 설정
+                //JWT는 키가 있으면 , JWT토큰에 해커가 장난질 쳤는지 안쳤는지 알 수 있음
+                //홰당 키는 JWT온라인에 등록을 해줘야함
+                .signWith(SignatureAlgorithm.HS512,
+                        "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890")
+                .compact();
+    }
+}
---
+++ src/test/java/com/ll/restapi/global/util/jwt/JwtUtilTest.java
@@ -0,0 +1,21 @@
+package com.ll.restapi.global.util.jwt;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtUtilTest {
+    @Test
+    @DisplayName("t1")
+    void t1() {
+        Map< String, String > data = Map.of("name", "홍길동", "age", "22");
+        String jwtToken = JwtUtil.encode(data);
+
+        System.out.println(jwtToken);
+        assertThat(jwtToken).isNotNull();
+    }
+
+}
